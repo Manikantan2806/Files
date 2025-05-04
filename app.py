5e286db363a320596ce4c7ea4007ac03956dc8bb
Manikantan2806 <manic8892@gmail.com>
2025-05-04 13:32:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ if f1 is not None:
     st.write(filename)
     df=pd.read_excel(filename,)
 else:
-    df=pd.read_excel("https://docs.google.com/spreadsheets/d/1sdESfx8Ouzgoa96PkSelguM6fpIA962P/edit?usp=drive_link&ouid=116202969749852348106&rtpof=true&sd=true")
-col1, col2 =st.columns((2))
+    error
 df["SALEDATE"] = pd.to_datetime(df["SALEDATE"])
 
 # Get min and max date from SALEDATE
